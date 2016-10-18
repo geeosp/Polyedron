d475d8956bfef4d5e08c16b5a76178da96b4781e
Luís Delgado <lhds@cin.ufpe.br>
2016-10-17 21:19:48 -0300

    Camera is moving

---
+++ Polyedron/Assets/Scripts/MainCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+    float distance = 0.1F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += transform.right * distance;
+    }
+}
