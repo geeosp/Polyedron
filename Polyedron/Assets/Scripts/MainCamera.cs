ce4f6627e676da6cc0c7b7132ca380b2b685c3b9
Luís Delgado <lhds@cin.ufpe.br>
2016-10-19 09:54:53 -0300

    Optimizing time of movement

--- Polyedron/Assets/Scripts/MainCamera.cs
+++ Polyedron/Assets/Scripts/MainCamera.cs
@@ -12,7 +12,7 @@ public class MainCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Time.timeSinceLevelLoad % 10 == 0 && transform.position.x <= 11)
+        if (Time.timeSinceLevelLoad % 10 == 1 && transform.position.x <= 11)
         {
             transform.position += transform.right * distance;
         }
