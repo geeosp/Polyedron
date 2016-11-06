b26f05d2591e2b4737b3931e8a3ae4a5c53c8af6
Luís Delgado <lhds@cin.ufpe.br>
2016-11-06 09:45:36 -0300

    Power button

--- Polyedron/Assets/Scripts/MainCamera.cs
+++ Polyedron/Assets/Scripts/MainCamera.cs
@@ -5,9 +5,23 @@ public class MainCamera : MonoBehaviour {
 
     float distance = 0.01F;
     int mod;
+    bool show;
+    public Texture btnTexture;
 
-	// Use this for initialization
-	void Start () {
+    void OnGUI()
+    {
+        if (!btnTexture)
+        {
+            Debug.LogError("Please assign a texture on the inspector");
+            return;
+        }
+        if (GUI.Button(new Rect(Screen.width-100, 10, 90, 90), btnTexture))
+            Debug.Log("Clicked the button with an image");
+            
+    }
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
