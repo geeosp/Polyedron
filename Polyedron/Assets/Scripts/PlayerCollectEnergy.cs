f6429b80d43c0aaa6f05a35d1c6178fa68065831
Dimas Albuquerque <Dimas Albuquerque>
2016-12-07 00:35:52 -0300

    Erros corrigidos, energy coletando

--- Polyedron/Assets/Scripts/PlayerCollectEnergy.cs
+++ Polyedron/Assets/Scripts/PlayerCollectEnergy.cs
@@ -3,15 +3,11 @@ using System.Collections;
 
 public class PlayerCollectEnergy : MonoBehaviour
 {
-	// Use this for initialization
-	void Start () {
-
-	}
-
 	void OnCollisionEnter2D(Collision2D col) {
 		Debug.Log("Collision: " + col.gameObject.name);
 		if (col.gameObject.tag == "Player") {
 			Destroy(gameObject);
+			DataManagement.datamanagement.increaseEnergy();
 		}
 	}
 
